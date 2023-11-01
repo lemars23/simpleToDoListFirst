15b8ca36f4a9512f5233255f04dc230cad6ab9c0
Alisher <alishersmagylov@gmail.com>
2023-11-01 12:15:27 +0600

    Add functions

--- js/script.js
+++ js/script.js
@@ -5,88 +5,136 @@ document.addEventListener("DOMContentLoaded", event => {
     const addButton = document.querySelector(".todo_button");
     const addForm = document.querySelector(".todo_add_form");
     const backList = document.querySelector(".todo_add_back>span");
-    let newArrayTasks = [];
 
     todoList.addEventListener("click", changeList);
-    todoList.addEventListener("click", event => changeChecked(event, newArrayTasks));
     addButton.addEventListener("click", toggleAddModal);
     backList.addEventListener("click", toggleAddModal);
-    addForm.addEventListener("submit", event => addTasks(event, newArrayTasks, todoList));
+
+    addForm.addEventListener("submit", event => addTask(event) );
+    addForm.addEventListener("submit", () => showTasks(todoList));
+    todoList.addEventListener("click", event => changeLSChecked(event));
+
+    showTasks(todoList);
 });
 
+// Добавление и выполнение задач
+// Добавить задание, добавляет в localStorage
+function addTask(event) {
+    event.preventDefault();
 
-function showArrayTasks(arrayTasks, list) {
+    // Получает введенное значение из добавления задания
+    const inputTask = document.querySelector(".todo_add_text").value;
+    // Устанавливает время (Пример => 09:06)
+    const time = getZeroTime(new Date().getHours()) + ":" + getZeroTime(new Date().getMinutes());
+    // Получает максимальое id и увеличивает его
+    const position = (getId()) + 1;
+    // Устанавливает значение в localStorage
+    localStorage.setItem(position, JSON.stringify({id: position, task: inputTask, time:time, checked: false}));
+}
+// Показывает задачи на главной страницы
+function showTasks(list) {
+    // Сброс списка
     list.innerHTML = "";
-
-    for(let task of arrayTasks) {
-        task = JSON.parse(task);
-
+    // Выводит задачи на главную страницу, задачи отсортированны по id
+    for(let key of getArrayTasks()) {
         list.innerHTML += `
-        <li class="todo_item item-${task.id}">
+        <li class="todo_item item-${key.id}">
             <label class="todo_labels">
                 <input
                     type="checkbox"
                     class="todo_checkboxes"
                     name="todo_checkboxes"
                 />
-                <span class="todo_fakebox"></span>
-                <span class="todo_text">${task.task}</span>
+                <span class="todo_fakebox ${key.checked ? "todo_border todo_opacity" : ""}"></span>
+                <span class="todo_text ${key.checked ? "todo_line_through color_grey" : ""}">${key.task}</span>
             </label>
-            <div class="todo_time color_red">
-                <span>${task.time}</span>
+            <div class="todo_time ${key.checked ? "color_grey" : ""}">
+                <span>${key.time}</span>
             </div>
         </li>
         `;
     }
 }
-function addTasks(event, array, list) {
-    event.preventDefault();
 
-    const value = event.target.firstElementChild.value;
-    const time = (new Date()).getHours() + ":" + (new Date()).getMinutes();
-    let id = Number(array.length) + 1;
-    array.push(JSON.stringify({
-        "id": id,
-        "task": value,
-        "time": time,
-        "checked": false
-    }));
-
-    // console.log(array);
-    showArrayTasks(array, list);
+// Получает максимальное id из localStorage
+function getId(arrayIds = []) {
+    if(localStorage.length > 0) {
+        for(let key in localStorage) {
+            if (!localStorage.hasOwnProperty(key)) {
+                continue;
+            }
+            arrayIds.push(JSON.parse(localStorage[key]).id);
+        }
+        return Math.max.apply(null, arrayIds);
+    }
+    return 0;
 }
-function changeChecked(event, array) {
+
+// Изменить параметр checked в localStorage
+function changeLSChecked(event) {
     if(event.target && event.target.matches("input.todo_checkboxes")) {
         const id = event.target.parentElement.parentElement.classList.item(1).split("-")[1];
-
-        for(let item of array) {
-            item = JSON.parse(item);
-            
-            if(item.id == id) {
-                item.checked = true;
+        for(let key of getArrayTasks()) {
+            if(key.id === (+id)) {
+                const toggleChecked = getTaskByIdFromStorage(key.id).checked ? false : true;
+                localStorage.setItem(key.id, JSON.stringify({id: key.id, task: key.task, time: key.time, checked: toggleChecked}))
             }
         }
     }
-    return array;
+}
+// Получить задание по id в localStorage
+function getTaskByIdFromStorage(id) {
+    for(let key in localStorage) {
+        key = JSON.parse(localStorage[key]);
+        if(key.id == id) {
+            return key;
+        }
+    }
+}
+// Добавляет объекты в массив из localStorage, парсит JSON
+function getArrayTasks(newArr = []) {
+    for(let key in localStorage) {
+        if (!localStorage.hasOwnProperty(key)) {
+            continue;
+        }
+        newArr.push(JSON.parse(localStorage[key]));
+    }
+    return newArr.sort((a, b) => a.id - b.id);
+}
+// Получить id в массиве заданий
+function getTaskByIdFromArray(event) {
+    if(event.target && event.target.matches("input.todo_checkboxes")) {
+        return event.target.parentElement.parentElement.classList.item(1).split("-")[1];
+    }
 }
 
+// Визуализация
+// Добавление и удаление классов при нажатии, на input или label
 function changeList(event) {
     if(event.target && event.target.matches("input.todo_checkboxes")) {
         const label = event.target.parentElement;
         toggleClass(label.lastElementChild, "todo_line_through");
         toggleClass(label.lastElementChild, "color_grey");
         toggleClass(label.nextElementSibling, "color_grey");
-        toggleClass(label.children[1], "todo_border");
     }
 }
+// Включение или отключение модального окна "добавления задач"
 function toggleAddModal(event) {
     event.preventDefault();
     toggleClass(document.querySelector(".todo_add_modal"), "todo_add_turn");
 }
+// Добавление и удаление класса
 function toggleClass(element, enemyClass) {
     if(element.classList.contains(enemyClass)) {
         element.classList.remove(enemyClass);
     } else {
         element.classList.add(enemyClass);
     }
+}
+// Добавление нуля если меньше 1 числа
+function getZeroTime(number) {
+    if(("" + number).length < 2) {
+        return `0${number}`;
+    }
+    return number;
 }
